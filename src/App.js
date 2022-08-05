5beda1643646d65e32b15de38c257f471d054d78
Caleb <caleb@calebcurry.com>
2022-08-05 12:40:02 -0400

    props example

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 import Employee from './components/Employee';
 
 function App() {
-    console.log('we are about to list the employees');
     const showEmployees = true;
     return (
         <div className="App">
-            {console.log('inside the return')}
             {showEmployees ? (
                 <>
-                    <Employee />
-                    <Employee />
-                    <Employee />
-                    <Employee />
-                    <Employee />
+                    <Employee name="Caleb" role="Intern" />
+                    <Employee name="Abby" />
+                    <Employee name="John" />
                 </>
             ) : (
                 <p>You cannot see the employees</p>
