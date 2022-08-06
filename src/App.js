e913c0c01b423e57d22f8456e0dc7d7783da3d7c
Caleb <caleb@calebcurry.com>
2022-08-06 14:18:18 -0400

    about to add the modal. So far it just has organizzed employees

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
     const [role, setRole] = useState('dev');
+    const [employees, setEmployees] = useState([
+        {
+            name: 'Caleb',
+            role: 'YouTube Sensation',
+            img: 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg',
+        },
+        {
+            name: 'Sal',
+            role: 'Manager',
+            img: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg',
+        },
+        {
+            name: 'John',
+            role: 'Director of Eng.',
+            img: 'https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg',
+        },
+        {
+            name: 'Melanie',
+            role: 'Software Engineer',
+            img: 'https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg',
+        },
+        {
+            name: 'Corey',
+            role: 'The Devops Guy',
+            img: 'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg',
+        },
+        {
+            name: 'Jake',
+            role: 'Senior',
+            img: 'https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg',
+        },
+    ]);
+
     const showEmployees = true;
     return (
         <div className="App">
@@ -17,48 +51,17 @@ function App() {
                         }}
                     />
                     <div className="flex flex-wrap justify-center">
-                        <Employee
-                            name="Caleb"
-                            role="Intern"
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="Abby"
-                            role={role}
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="John"
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="Caleb"
-                            role="Intern"
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="Abby"
-                            role={role}
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="John"
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="Caleb"
-                            role="Intern"
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="Abby"
-                            role={role}
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
-                        <Employee
-                            name="John"
-                            img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
-                        />
+                        {employees.map((employee) => {
+                            console.log(uuidv4());
+                            return (
+                                <Employee
+                                    key={uuidv4()}
+                                    name={employee.name}
+                                    role={employee.role}
+                                    img={employee.img}
+                                />
+                            );
+                        })}
                     </div>
                 </>
             ) : (
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -11,7 +11,7 @@ function Employee(props) {
                     <p class="text-slate-500 font-medium">{props.role}</p>
                 </div>
                 <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                    Message
+                    Update
                 </button>
             </div>
         </div>
