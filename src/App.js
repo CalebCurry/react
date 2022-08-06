133e24f7f24b8d61f65d4ef95dd4e54a3b0e6b00
Caleb <caleb@calebcurry.com>
2022-08-06 09:51:55 -0400

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+    const [role, setRole] = useState('dev');
     const showEmployees = true;
     return (
         <div className="App">
             {showEmployees ? (
                 <>
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setRole(e.target.value);
+                        }}
+                    />
                     <Employee name="Caleb" role="Intern" />
-                    <Employee name="Abby" />
+                    <Employee name="Abby" role={role} />
                     <Employee name="John" />
                 </>
             ) : (
