a67fb256bd11e3a92485e22118e1ac5e6fc6c94d
Caleb <caleb@calebcurry.com>
2022-08-07 06:33:10 -0400

    change class to className

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,16 +1,18 @@
 function Employee(props) {
     return (
-        <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img
-                class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+                className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
                 src={props.img}
             />
-            <div class="text-center space-y-2 sm:text-left">
-                <div class="space-y-0.5">
-                    <p class="text-lg text-black font-semibold">{props.name}</p>
-                    <p class="text-slate-500 font-medium">{props.role}</p>
+            <div className="text-center space-y-2 sm:text-left">
+                <div className="space-y-0.5">
+                    <p className="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p className="text-slate-500 font-medium">{props.role}</p>
                 </div>
-                <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
                     Update
                 </button>
             </div>
