5aa1fb544b29fa5647eceac7052a0ab0dace0890
Caleb <caleb@calebcurry.com>
2022-08-07 07:49:14 -0400

    added modal and styled form

---
+++ src/components/EditEmployee.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function EditEmployee() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <button
+                onClick={handleShow}
+                className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+            >
+                Update
+            </button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Update Employee</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form id="editmodal" className="w-full max-w-sm">
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label
+                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    for="name"
+                                >
+                                    Full Name
+                                </label>
+                            </div>
+                            <div className="md:w-2/3">
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="name"
+                                    type="text"
+                                    value="Jane Doe"
+                                />
+                            </div>
+                        </div>
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label
+                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    for="role"
+                                >
+                                    Role
+                                </label>
+                            </div>
+                            <div className="md:w-2/3">
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="role"
+                                    type="text"
+                                    value="Jane Doe"
+                                />
+                            </div>
+                        </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button
+                        className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
+                        onClick={handleClose}
+                    >
+                        Close
+                    </button>
+                    <button
+                        className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                        form="editmodal"
+                    >
+                        Update
+                    </button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default EditEmployee;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,3 +1,5 @@
+import EditEmployee from './EditEmployee';
+
 function Employee(props) {
     return (
         <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
@@ -12,9 +14,8 @@ function Employee(props) {
                     </p>
                     <p className="text-slate-500 font-medium">{props.role}</p>
                 </div>
-                <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                    Update
-                </button>
+
+                <EditEmployee />
             </div>
         </div>
     );
