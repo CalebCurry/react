50094f873ed09e0a368b12fcdab4335853b169b1
Caleb <caleb@calebcurry.com>
2022-08-11 11:15:56 -0400

    introduce pages and children props

--- src/App.js
+++ src/App.js
@@ -5,103 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
+import Employees from './pages/Employees';
 
 function App() {
-    const [role, setRole] = useState('dev');
-    const [employees, setEmployees] = useState([
-        {
-            id: 1,
-            name: 'Caleb',
-            role: 'YouTube Sensation',
-            img: 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg',
-        },
-        {
-            id: 2,
-            name: 'Sal',
-            role: 'Manager',
-            img: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg',
-        },
-        {
-            id: 3,
-            name: 'John',
-            role: 'Director of Eng.',
-            img: 'https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg',
-        },
-        {
-            id: 4,
-            name: 'Melanie',
-            role: 'Software Engineer',
-            img: 'https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg',
-        },
-        {
-            id: 5,
-            name: 'Corey',
-            role: 'The Devops Guy',
-            img: 'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg',
-        },
-        {
-            id: 6,
-            name: 'Jake',
-            role: 'Senior',
-            img: 'https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg',
-        },
-    ]);
-
-    function updateEmployee(id, newName, newRole) {
-        const updatedEmployees = employees.map((employee) => {
-            if (id == employee.id) {
-                return { ...employee, name: newName, role: newRole };
-            }
-
-            return employee;
-        });
-        setEmployees(updatedEmployees);
-    }
-
-    function newEmployee(name, role, img) {
-        const newEmployee = {
-            id: uuidv4(),
-            name: name,
-            role: role,
-            img: img,
-        };
-        setEmployees([...employees, newEmployee]);
-    }
-
-    const showEmployees = true;
     return (
-        <div className="App bg-gray-300 min-h-screen">
-            <Header />
-            {showEmployees ? (
-                <>
-                    <div className="flex flex-wrap justify-center my-2">
-                        {employees.map((employee) => {
-                            const editEmployee = (
-                                <EditEmployee
-                                    id={employee.id}
-                                    name={employee.name}
-                                    role={employee.role}
-                                    updateEmployee={updateEmployee}
-                                />
-                            );
-                            return (
-                                <Employee
-                                    key={employee.id}
-                                    id={employee.id}
-                                    name={employee.name}
-                                    role={employee.role}
-                                    img={employee.img}
-                                    editEmployee={editEmployee}
-                                />
-                            );
-                        })}
-                    </div>
-                    <AddEmployee newEmployee={newEmployee} />
-                </>
-            ) : (
-                <p>You cannot see the employees</p>
-            )}
-        </div>
+        <Header>
+            <Employees />
+        </Header>
     );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ');
 }
 
-export default function Header() {
+export default function Header(props) {
     return (
         <Disclosure as="nav" className="bg-gray-800">
             {({ open }) => (
@@ -172,6 +172,7 @@ export default function Header() {
                             ))}
                         </div>
                     </Disclosure.Panel>
+                    {props.children}
                 </>
             )}
         </Disclosure>
