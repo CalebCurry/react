1bba44fbb67880777390df8073190d6a5f90e94a
Caleb <caleb@calebcurry.com>
2022-08-11 08:54:23 -0400

    basic header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
+import Header from './components/Header';
 
 function App() {
     const [role, setRole] = useState('dev');
@@ -69,17 +70,11 @@ function App() {
 
     const showEmployees = true;
     return (
-        <div className="App">
+        <div className="App bg-gray-300 min-h-screen">
+            <Header />
             {showEmployees ? (
                 <>
-                    <input
-                        type="text"
-                        onChange={(e) => {
-                            console.log(e.target.value);
-                            setRole(e.target.value);
-                        }}
-                    />
-                    <div className="flex flex-wrap justify-center">
+                    <div className="flex flex-wrap justify-center my-2">
                         {employees.map((employee) => {
                             const editEmployee = (
                                 <EditEmployee
