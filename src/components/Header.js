3e40ddac3a3b3eb527ee848f9ce009afd72fb855
Caleb <caleb@calebcurry.com>
2022-08-14 06:48:31 -0400

    mobile styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,168 +17,106 @@ function classNames(...classes) {
 
 export default function Header(props) {
     return (
-        <Disclosure as="nav" className="bg-gray-800">
-            {({ open }) => (
-                <>
-                    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                        <div className="relative flex items-center justify-between h-14">
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="sr-only">
-                                        Open main menu
-                                    </span>
-                                    {open ? (
-                                        <XIcon
-                                            className="block h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    ) : (
-                                        <MenuIcon
-                                            className="block h-6 w-6"
-                                            aria-hidden="true"
-                                        />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
-                            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="hidden sm:block sm:ml-6">
-                                    <div className="flex space-x-4">
-                                        {/*className={classNames(
+        <>
+            <Disclosure as="nav" className="bg-gray-800">
+                {({ open }) => (
+                    <>
+                        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                            <div className="relative flex items-center justify-between h-14">
+                                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                    {/* Mobile menu button*/}
+                                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                        <span className="sr-only">
+                                            Open main menu
+                                        </span>
+                                        {open ? (
+                                            <XIcon
+                                                className="block h-6 w-6"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <MenuIcon
+                                                className="block h-6 w-6"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                    </Disclosure.Button>
+                                </div>
+                                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                    <div className="hidden sm:block sm:ml-6">
+                                        <div className="flex space-x-4">
+                                            {/*className={classNames(
                                                     item.current
                                                         ? 'no-underline '
                                                         : 'no-underline',
                                                     ''
                                                 )}*/}
-                                        {navigation.map((item) => (
-                                            <NavLink
-                                                key={item.name}
-                                                to={item.href}
-                                                className={({ isActive }) => {
-                                                    return (
-                                                        'px-3 py-2 rounded-md text-sm font-medium no-underline ' +
-                                                        (!isActive
-                                                            ? ' text-gray-300 hover:bg-gray-700 hover:text-white'
-                                                            : 'bg-gray-900 text-white')
-                                                    );
-                                                }}
-                                            >
-                                                {item.name}
-                                            </NavLink>
-                                        ))}
+                                            {navigation.map((item) => (
+                                                <NavLink
+                                                    key={item.name}
+                                                    to={item.href}
+                                                    className={({
+                                                        isActive,
+                                                    }) => {
+                                                        return (
+                                                            'px-3 py-2 rounded-md text-sm font-medium no-underline ' +
+                                                            (!isActive
+                                                                ? ' text-gray-300 hover:bg-gray-700 hover:text-white'
+                                                                : 'bg-gray-900 text-white')
+                                                        );
+                                                    }}
+                                                >
+                                                    {item.name}
+                                                </NavLink>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <button
-                                    type="button"
-                                    className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                                >
-                                    <span className="sr-only">
-                                        View notifications
-                                    </span>
-                                    <BellIcon
-                                        className="h-6 w-6"
-                                        aria-hidden="true"
-                                    />
-                                </button>
-
-                                {/* Profile dropdown */}
-                                <Menu as="div" className="ml-3 relative">
-                                    <div>
-                                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                            <span className="sr-only">
-                                                Open user menu
-                                            </span>
-                                        </Menu.Button>
-                                    </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
+                                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                    <button
+                                        type="button"
+                                        className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                     >
-                                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active
-                                                                ? 'bg-gray-100'
-                                                                : '',
-                                                            'block px-4 py-2 text-sm text-gray-700'
-                                                        )}
-                                                    >
-                                                        Sign out
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
+                                        <span className="sr-only">
+                                            View notifications
+                                        </span>
+                                        <BellIcon
+                                            className="h-6 w-6"
+                                            aria-hidden="true"
+                                        />
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <Disclosure.Panel className="sm:hidden">
-                        <div className="px-2 pt-2 pb-3 space-y-1">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current
-                                            ? 'bg-gray-900 text-white'
-                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block px-3 py-2 rounded-md text-base font-medium'
-                                    )}
-                                    aria-current={
-                                        item.current ? 'page' : undefined
-                                    }
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
-                            ))}
-                        </div>
-                    </Disclosure.Panel>
+                        <Disclosure.Panel className="sm:hidden">
+                            <div className="px-2 pt-2 pb-3 space-y-1">
+                                {navigation.map((item) => (
+                                    <NavLink
+                                        key={item.name}
+                                        to={item.href}
+                                        className={({ isActive }) => {
+                                            return (
+                                                'block px-3 py-2 rounded-md text-base font-medium no-underline ' +
+                                                (!isActive
+                                                    ? ' text-gray-300 hover:bg-gray-700 hover:text-white'
+                                                    : 'bg-gray-900 text-white')
+                                            );
+                                        }}
+                                    >
+                                        {item.name}
+                                    </NavLink>
+                                ))}
+                            </div>
+                        </Disclosure.Panel>
+                    </>
+                )}
+            </Disclosure>
+            <div className="bg-gray-300">
+                <div className="max-w-7xl mx-auto min-h-screen px-2 py-2">
                     {props.children}
-                </>
-            )}
-        </Disclosure>
+                </div>
+            </div>
+        </>
     );
 }
--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -69,10 +69,10 @@ function Employees() {
 
     const showEmployees = true;
     return (
-        <div className="App bg-gray-300 min-h-screen">
+        <div className="">
             {showEmployees ? (
                 <>
-                    <div className="flex flex-wrap justify-center my-2">
+                    <div className="flex flex-wrap justify-center">
                         {employees.map((employee) => {
                             const editEmployee = (
                                 <EditEmployee
