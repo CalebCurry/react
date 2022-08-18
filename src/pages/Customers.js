c4a74d60915c51a5de9205baee9284a3f243d036
Caleb <caleb@calebcurry.com>
2022-08-18 07:58:21 -0400

    request customers from custom backend

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,23 @@
+import { useEffect, useState } from 'react';
 export default function Customers() {
-    return <h1>Hello there!</h1>;
+    const [customers, setCustomers] = useState();
+    useEffect(() => {
+        console.log('Fetching...');
+        fetch('http://localhost:8000/api/customers/')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setCustomers(data.customers);
+            });
+    }, []);
+    return (
+        <>
+            <h1>Here are our customer:</h1>
+            {customers
+                ? customers.map((customer) => {
+                      return <p>{customer.name}</p>;
+                  })
+                : null}
+        </>
+    );
 }
