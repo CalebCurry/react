da529ccac04b96df21c7cab41bbcafb7188acfd8
Caleb <caleb@calebcurry.com>
2022-08-20 06:54:49 -0400

    fetch to add data but frontend work still needing done

---
+++ src/components/AddCustomer.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function AddCustomer(props) {
+    const [name, setName] = useState('');
+    const [industry, setIndustry] = useState('');
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <button
+                onClick={handleShow}
+                className="block mx-auto m-2 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            >
+                + Add Customer
+            </button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Customer</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            setName('');
+                            setIndustry('');
+                            props.newCustomer(name, industry);
+                        }}
+                        id="editmodal"
+                        className="w-full max-w-sm"
+                    >
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label
+                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    for="name"
+                                >
+                                    Name
+                                </label>
+                            </div>
+                            <div className="md:w-2/3">
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="name"
+                                    placeholder="Google"
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => {
+                                        setName(e.target.value);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label
+                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    for="industry"
+                                >
+                                    Industry
+                                </label>
+                            </div>
+                            <div className="md:w-2/3">
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="industry"
+                                    placeholder="Computing"
+                                    type="text"
+                                    value={industry}
+                                    onChange={(e) => {
+                                        setIndustry(e.target.value);
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button
+                        className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
+                        onClick={handleClose}
+                    >
+                        Close
+                    </button>
+                    <button
+                        className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={handleClose}
+                        form="editmodal"
+                    >
+                        Add
+                    </button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import AddCustomer from '../components/AddCustomer';
 import { baseUrl } from '../shared';
 
 export default function Customers() {
@@ -12,9 +13,34 @@ export default function Customers() {
                 setCustomers(data.customers);
             });
     }, []);
+    function newCustomer(name, industry) {
+        const data = { name: name, industry: industry };
+        const url = baseUrl + 'api/customers/';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                //assume the add was succesful
+                //hide the modal
+                //make sure the list is updated appropriately
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
     return (
         <>
-            <h1>Here are our customer:</h1>
+            <h1>Here are our customers:</h1>
             <ul>
                 {customers
                     ? customers.map((customer) => {
@@ -28,6 +54,7 @@ export default function Customers() {
                       })
                     : null}
             </ul>
+            <AddCustomer newCustomer={newCustomer} />
         </>
     );
 }
